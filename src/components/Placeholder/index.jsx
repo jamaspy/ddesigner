97cfef7ec7e0799ed9d976b75a5dc049d932600f
jamesaspinall <james.aspinall@agridigital.io>
2021-05-20 18:35:28 +1000

    setup

---
+++ src/components/Placeholder/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import * as styles from "./styles.module.scss";
+const Placeholder = () => {
+  return (
+    <div>
+      <p className={styles.title}>Hello World x</p>
+    </div>
+  );
+};
+
+export default Placeholder;
